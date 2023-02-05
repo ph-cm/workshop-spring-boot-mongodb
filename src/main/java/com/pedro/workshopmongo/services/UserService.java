6e97a1fd54621941567d5d73d39da73026618802
Pedro <trabalho.prog.pedro@gmail.com>
2023-02-05 16:45:27 -0300

    Conectando mongoDB com repository e service

---
+++ src/main/java/com/pedro/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.pedro.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedro.workshopmongo.domain.User;
+import com.pedro.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
