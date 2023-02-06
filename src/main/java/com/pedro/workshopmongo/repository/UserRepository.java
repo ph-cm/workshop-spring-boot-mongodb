2aa9aefa520bd8c68b503b9ed48d79ff069563e9
Pedro <trabalho.prog.pedro@gmail.com>
2023-02-06 03:37:22 -0300

    Instanciação da base de dados

--- src/main/java/com/pedro/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/pedro/workshopmongo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.pedro.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
